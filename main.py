206cae32662b5d9aaed3517a6acde245d4ebe87f
Rpgale43 <rpgale43@gmail.com>
2025-03-05 21:41:18 -0600

    added a circle and player class, draw player to the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     #Initialize pygame, clock, and screen size
@@ -7,6 +8,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(1):
         #Fills screen with color black
@@ -16,6 +18,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player_ship.draw(screen)
 
         #Update the screen
         pygame.display.flip()
